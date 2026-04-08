c2e86971e082dd7713051659d7338cecfeeab967
Tom Gries <kontakt.tomgries@gmail.com>
2026-04-08 16:51:10 +0200

    chance db file type

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb"
 
-const client = new MongoClient(process.env.MONGO_URL, {
+const client = new MongoClient(process.env.MONGO_URL ?? "", {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
