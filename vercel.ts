6bd4f8bdcf18f28a6fc4ab39613f66a95e942bed
Tom Gries <kontakt.tomgries@gmail.com>
2026-04-08 19:55:02 +0200

    test18

---
+++ vercel.ts
@@ -0,0 +1,11 @@
+import type {
+  VercelConfig
+} from '@vercel/config/v1';
+
+export const config: VercelConfig = {
+  framework: 'nextjs',
+  functions: {
+    'api /**/*.ts': {
+      includeFiles: 'lib/**'
+    }
+  }
