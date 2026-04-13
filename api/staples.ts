2f20f4b01c7dd51a530ff9a3fc13d4ec94b1eab8
Tom Gries <kontakt.tomgries@gmail.com>
2026-04-13 17:03:48 +0200

    Change stable return value

--- api/staples.ts
+++ api/staples.ts
@@ -10,7 +10,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const handler = methodHandler.init({
       "GET": async (req: VercelRequest, res: VercelResponse) => {
         const allStaples = await stables.getAll()
-        return res.json(allStaples)
+        return res.json(allStaples.getValue())
       },
       "POST": async (req: VercelRequest, res: VercelResponse) => {
         const stable = namedStable.setValue(req.body)
--- lib/types/ObjectHandler.ts
+++ lib/types/ObjectHandler.ts
@@ -6,7 +6,7 @@ export interface ObjectHandler<T = NamedObject> {
   dbConnection: Collection<Document> | null
   init: () => Promise<ObjectHandler<T>>
   getbyIndex: (index: number) => T | null
-  getAll: () => Promise<T | null>
+  getAll: () => Promise<T>
   add: (value: T) => void
   removeByIndex: (index: number) => void
   changeByIndex: (index: number, value: T) => void
